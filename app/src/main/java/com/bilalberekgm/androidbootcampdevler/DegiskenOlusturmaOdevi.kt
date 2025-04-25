e219294479c45d299cd09db1713ee35ed782b064
bilalceng <bilalberekgm@gmail.com>
2025-04-25 13:46:50 +0300

    ekleme: Degisken degistirme odevi eklendi.

---
+++ app/src/main/java/com/bilalberekgm/androidbootcampdevler/DegiskenOlusturmaOdevi.kt
@@ -0,0 +1,47 @@
+package com.bilalberekgm.androidbootcampdevler
+
+fun main() {
+
+    // 1.sütun
+    val sehir = "Batman"
+    val ulke = "Türkiye"
+    val telefon = "+905374118597"
+    val postaKodu = "72500"
+    val email = "bilalberekgmbuss@gmail.com"
+    val meslek = "Yazılım mühendisi"
+    val stokMiktari = 34567
+    val musteriAdi = "Ahmet"
+    val bakiye = 345.0
+    val dogumGunu = "12.09.1987"
+    val maas = 33420.50
+    val medeniDurum = "Bekar"
+
+
+    // 2.sütun
+    val odemeTarihi = "12.03.2026"
+    val odeme = 1890.78
+    val siparisAdeti = 3
+    val arabaModeli = "BMV"
+    val kitapAdi = "Olasılıksız"
+    val yayinlanmaTarihi = "23.03.2025"
+    val indirimMiktari = "%50"
+    val odaSayisi = 3
+    val enlem = "40.7128° N"
+    val boylam = "74.0060° W"
+    val urunAdi = "Cüzdan"
+    val yemekFiyati = 350
+
+
+    // 3.sütun
+    var marka = "Nike"
+    var muzikAdi = "Kış Güneşi"
+    var videoSuresi = "2:45.32"
+    var urunPuani = 4.3
+    var resimAdi = "İnci Küpeli Kız"
+    var dosyaFormati = "zip"
+    var renk = "Kırmızı"
+    var renkKodu = "#FF0000"
+    var telefonModeli = "Samsung J7"
+    var ekranBoyutu = "700x670"
+    var agirlik = 67.89
+}
