113410b1361930d1c09d90dec1a16d4313730ea2
bilalceng <bilalberekgm@gmail.com>
2025-04-25 14:31:22 +0300

    test: Degiskenler main methodda cagrildi ve test edildi.

--- app/src/main/java/com/bilalberekgm/androidbootcampdevler/DegiskenOlusturmaOdevi.kt
+++ app/src/main/java/com/bilalberekgm/androidbootcampdevler/DegiskenOlusturmaOdevi.kt
@@ -1,8 +1,7 @@
 package com.bilalberekgm.androidbootcampdevler
 
 fun main() {
-
-    // 1.sütun
+    // 1. Sütun Değişkenleri
     val sehir = "Batman"
     val ulke = "Türkiye"
     val telefon = "+905374118597"
@@ -16,8 +15,7 @@ fun main() {
     val maas = 33420.50
     val medeniDurum = "Bekar"
 
-
-    // 2.sütun
+    // 2. Sütun Değişkenleri
     val odemeTarihi = "12.03.2026"
     val odeme = 1890.78
     val siparisAdeti = 3
@@ -31,8 +29,7 @@ fun main() {
     val urunAdi = "Cüzdan"
     val yemekFiyati = 350
 
-
-    // 3.sütun
+    // 3. Sütun Değişkenleri
     var marka = "Nike"
     var muzikAdi = "Kış Güneşi"
     var videoSuresi = "2:45.32"
@@ -44,4 +41,46 @@ fun main() {
     var telefonModeli = "Samsung J7"
     var ekranBoyutu = "700x670"
     var agirlik = 67.89
+
+    // Çıktılar (Yorum Satırlarıyla)
+    println("1. SÜTUN:")
+    println("Şehir: $sehir") // Çıktı: Şehir: Batman
+    println("Ülke: $ulke") // Çıktı: Ülke: Türkiye
+    println("Telefon: $telefon") // Çıktı: Telefon: +905374118597
+    println("Posta Kodu: $postaKodu") // Çıktı: Posta Kodu: 72500
+    println("E-posta: $email") // Çıktı: E-posta: bilalberekgmbuss@gmail.com
+    println("Meslek: $meslek") // Çıktı: Meslek: Yazılım mühendisi
+    println("Stok Miktarı: $stokMiktari") // Çıktı: Stok Miktarı: 34567
+    println("Müşteri Adı: $musteriAdi") // Çıktı: Müşteri Adı: Ahmet
+    println("Bakiye: $bakiye ₺") // Çıktı: Bakiye: 345.0 ₺
+    println("Doğum Günü: $dogumGunu") // Çıktı: Doğum Günü: 12.09.1987
+    println("Maaş: $maas ₺") // Çıktı: Maaş: 33420.5 ₺
+    println("Medeni Durum: $medeniDurum") // Çıktı: Medeni Durum: Bekar
+
+    println("\n2. SÜTUN:")
+    println("Ödeme Tarihi: $odemeTarihi") // Çıktı: Ödeme Tarihi: 12.03.2026
+    println("Ödeme Tutarı: $odeme ₺") // Çıktı: Ödeme Tutarı: 1890.78 ₺
+    println("Sipariş Adeti: $siparisAdeti") // Çıktı: Sipariş Adeti: 3
+    println("Araba Modeli: $arabaModeli") // Çıktı: Araba Modeli: BMV
+    println("Kitap Adı: $kitapAdi") // Çıktı: Kitap Adı: Olasılıksız
+    println("Yayınlanma Tarihi: $yayinlanmaTarihi") // Çıktı: Yayınlanma Tarihi: 23.03.2025
+    println("İndirim Miktarı: $indirimMiktari") // Çıktı: İndirim Miktarı: %50
+    println("Oda Sayısı: $odaSayisi") // Çıktı: Oda Sayısı: 3
+    println("Enlem: $enlem") // Çıktı: Enlem: 40.7128° N
+    println("Boylam: $boylam") // Çıktı: Boylam: 74.0060° W
+    println("Ürün Adı: $urunAdi") // Çıktı: Ürün Adı: Cüzdan
+    println("Yemek Fiyatı: $yemekFiyati ₺") // Çıktı: Yemek Fiyatı: 350 ₺
+
+    println("\n3. SÜTUN:")
+    println("Marka: $marka") // Çıktı: Marka: Nike
+    println("Müzik Adı: $muzikAdi") // Çıktı: Müzik Adı: Kış Güneşi
+    println("Video Süresi: $videoSuresi") // Çıktı: Video Süresi: 2:45.32
+    println("Ürün Puanı: $urunPuani/5") // Çıktı: Ürün Puanı: 4.3/5
+    println("Resim Adı: $resimAdi") // Çıktı: Resim Adı: İnci Küpeli Kız
+    println("Dosya Formatı: $dosyaFormati") // Çıktı: Dosya Formatı: zip
+    println("Renk: $renk") // Çıktı: Renk: Kırmızı
+    println("Renk Kodu: $renkKodu") // Çıktı: Renk Kodu: #FF0000
+    println("Telefon Modeli: $telefonModeli") // Çıktı: Telefon Modeli: Samsung J7
+    println("Ekran Boyutu: $ekranBoyutu piksel") // Çıktı: Ekran Boyutu: 700x670 piksel
+    println("Ağırlık: $agirlik kg") // Çıktı: Ağırlık: 67.89 kg
 }
